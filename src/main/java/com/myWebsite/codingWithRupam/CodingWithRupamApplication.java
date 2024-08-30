04a5edb5d603b4bd7430fea19973d98caff6fc87
Rupam Das <rupamdas00001@gmail.com>
2024-08-30 23:17:49 +0530

    Initial commit - added Spring Boot application

---
+++ src/main/java/com/myWebsite/codingWithRupam/CodingWithRupamApplication.java
@@ -0,0 +1,13 @@
+package com.myWebsite.codingWithRupam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingWithRupamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingWithRupamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myWebsite/codingWithRupam/CodingWithRupamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myWebsite.codingWithRupam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingWithRupamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
